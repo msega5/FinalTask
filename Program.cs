60a76c59029781a5b3b877f913f5d3c2a876197a
Sergei Efanov <efanv_serg@mail.ru>
2023-06-26 10:54:15 +0300

    Добавил метод подсчёта строк и символов

--- Program.cs
+++ Program.cs
@@ -9,3 +9,24 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+
+void ArrayInit(string [] args)
+{
+    int index = 0;
+    string input = System.Console.ReadLine();
+    foreach (string word in input.Split(new char[] { ' ' }))
+    {
+       // System.Console.WriteLine("{0}: {1}", word, word.Length);
+        System.Console.WriteLine($"{word}: {word.Length}");
+        index++;
+    }
+    System.Console.WriteLine(index);
+}
+
+
+System.Console.WriteLine("Введите значения (буквы, числа, слова) через пробел и нажмите Enter");
+ArrayInit(args);
+//string[] array = new string[index - 1];
